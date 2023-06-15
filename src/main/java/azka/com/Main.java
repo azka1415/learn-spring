7f7b1543a8dd71b51ad89f2d889f88e4c5121359
azka1415 <rafifzaydan4@gmail.com>
2023-06-15 15:50:02 +0700

    cleaning

--- src/main/java/azka/com/Main.java
+++ src/main/java/azka/com/Main.java
@@ -3,20 +3,12 @@ package azka.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
-
-
-
-
-
 }
-
 
