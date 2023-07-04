37be94a55f26862212fde6748af6c2d161a83ec5
azka1415 <rafifzaydan4@gmail.com>
2023-07-04 23:58:50 +0700

    added check if exists with email

--- src/main/java/azka/com/customer/CustomerRepository.java
+++ src/main/java/azka/com/customer/CustomerRepository.java
@@ -3,5 +3,5 @@ package azka.com.customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-
+    boolean existsCustomerByEmail(String email);
 }
