d31bc233fd120e62514bab36a8f1bbc152a188ae
azka1415 <rafifzaydan4@gmail.com>
2023-07-04 16:03:35 +0700

    added basic columns not null

--- src/main/java/azka/com/customer/Customer.java
+++ src/main/java/azka/com/customer/Customer.java
@@ -1,12 +1,21 @@
 package azka.com.customer;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
 public class Customer {
+
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence",sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy =GenerationType.SEQUENCE, generator = "customer_id_sequence")
     private Integer id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private Integer age;
 
     public Customer(Integer id, String name, String email, Integer age) {
@@ -15,4 +24,6 @@ public class Customer {
         this.email = email;
         this.age = age;
     }
+    public Customer() {}
+
 }
