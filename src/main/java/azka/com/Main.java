757a033bfa0291e702a231a9db5fad2bd4f2c0d6
azka1415 <rafifzaydan4@gmail.com>
2023-07-04 23:21:43 +0700

    added commandline runner for testing data

--- src/main/java/azka/com/Main.java
+++ src/main/java/azka/com/Main.java
@@ -1,14 +1,31 @@
 package azka.com;
 
 
+import azka.com.customer.Customer;
+import azka.com.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
+    }
+
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+            Customer alex = new Customer("alex","alex@gmail.com",40);
+            Customer jamila = new Customer("jamila","jamila@gmail.com",21);
+            List<Customer> customers = List.of(alex,jamila);
+            customerRepository.saveAll(customers);
+        };
     }
 }
 
